32cf93d1c7293052b89d33cf54a40fabc61d66f6
Pat Hansen <patrick.hansen1618@gmail.com>
2018-02-10 18:10:51 -0700

    Create basic routing, components, and services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RecordsService } from './records.service';
+import { RecordsComponent } from './records/records.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordsComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    RecordsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
