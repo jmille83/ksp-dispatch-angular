e612fa5467503791639ed3c9b47e756c32119b95
Pat Hansen <patrick.hansen1618@gmail.com>
2019-08-20 14:30:57 +1000

    AI checkbox.

--- src/app/objects/record.ts
+++ src/app/objects/record.ts
@@ -14,6 +14,7 @@ export class Record {
     type: string = "10-50";
     typeLabel: string;
     traumaActivated: boolean = false;
+    ai: boolean = false;
 
     // Secondary information.
     hasSecondaryInfo: boolean;
